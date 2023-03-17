59571b2df3eb0a99ae5f7209e25ac00f96343452
ColinoFerrara1993 <ferrara.nicola.1993@gmail.com>
2023-03-17 14:27:03 +0100

    file

---
+++ geant4project/LXe_job/LXe_job.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export HOME="/lustrehome/nicolaferrara2106"
+source $HOME/.bashrc.sh
+source $HOME/env.txt
+$HOME/projects/geant4/geant4project/LXe_neutron_build/LXe_n $HOME/projects/geant4/geant4project/LXe_neutron_build/LXe.mac 
